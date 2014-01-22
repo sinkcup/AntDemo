2f52493f668472ddcb433c20c093809a2c399e36
sinkcup <sinkcup@163.com>
2014-01-22 13:41:01 +0800

    first version: app ok, auto update ok, ant build todo

---
+++ src/io/github/sinkcup/ant/MainActivity.java
@@ -0,0 +1,63 @@
+package io.github.sinkcup.ant;
+
+import com.umeng.analytics.MobclickAgent;
+import com.umeng.update.UmengUpdateAgent;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        UmengUpdateAgent.update(this);
+        setContentView(R.layout.activity_main);
+
+        try {
+            String packageName = getPackageName();
+            ((TextView) findViewById(R.id.package_name)).setText(packageName);
+
+            String versionCode = Integer.toString(getPackageManager()
+                    .getPackageInfo(getPackageName(), 0).versionCode);
+            ((TextView) findViewById(R.id.version_code)).setText(versionCode);
+
+            String versionName = getPackageManager().getPackageInfo(
+                    getPackageName(), 0).versionName;
+            ((TextView) findViewById(R.id.version_name)).setText(versionName);
+
+            ApplicationInfo ai = getPackageManager().getApplicationInfo(
+                    getPackageName(), PackageManager.GET_META_DATA);
+            String umengChannel = ai.metaData.get("UMENG_CHANNEL").toString();
+            ((TextView) findViewById(R.id.umeng_channel)).setText(umengChannel);
+
+        } catch (NameNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    public void onPause() {
+        super.onPause();
+        MobclickAgent.onPause(this);
+    }
+
+    public void onResume() {
+        super.onResume();
+        MobclickAgent.onResume(this);
+    }
+
+}
